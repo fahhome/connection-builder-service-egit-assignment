897b0665698d1bf9f343f6b2214ff0261943d271
fahhome <fahmidjobs@gmail.com>
2021-04-22 23:16:30 +0530

    JUnit Test Case

--- src/test/java/fahmid/islam/connectionbuilderservice/ConnectionBuilderServiceApplicationTests.java
+++ src/test/java/fahmid/islam/connectionbuilderservice/ConnectionBuilderServiceApplicationTests.java
@@ -1,13 +1,27 @@
 package fahmid.islam.connectionbuilderservice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import fahmid.islam.connectionbuilderservice.service.FlightScheduleService;
 
 @SpringBootTest
 class ConnectionBuilderServiceApplicationTests {
 
+	@Autowired
+	private FlightScheduleService flightScheduleService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testLoadDataForTestClass() {
+		assertEquals(2, flightScheduleService.getFlightSchedules().size());
 	}
 
 }
