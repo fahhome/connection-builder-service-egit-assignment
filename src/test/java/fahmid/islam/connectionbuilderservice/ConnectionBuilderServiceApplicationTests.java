d8476125d28efccbe40a0965ebf7bda3af0f7adc
fahhome <fahmidjobs@gmail.com>
2021-04-24 12:30:01 +0530

    18% coverage

--- src/test/java/fahmid/islam/connectionbuilderservice/ConnectionBuilderServiceApplicationTests.java
+++ src/test/java/fahmid/islam/connectionbuilderservice/ConnectionBuilderServiceApplicationTests.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import fahmid.islam.connectionbuilderservice.dto.JoinRequest;
 import fahmid.islam.connectionbuilderservice.dto.JoinResponse;
 import fahmid.islam.connectionbuilderservice.entity.FlightSchedule;
 import fahmid.islam.connectionbuilderservice.service.FlightScheduleService;
@@ -58,6 +59,49 @@ class ConnectionBuilderServiceApplicationTests {
 		assertEquals(1, lhm.size());
 
 	}
+
+	@Test
+	public void testJoinRequest(){
+		JoinRequest jr = new JoinRequest();
+
+		jr.setFromAirport("BOM");//Lombok Setter
+		jr.setToAirport("JFK");
+
+		List<JoinResponse> allConnections = flightScheduleService.getConnectingFlights();
+		List<JoinResponse> lhm = new LinkedList<>();
+
+		allConnections.stream().filter(row -> row != null)
+				.filter(row -> row.getFirstDepAirport().equals(jr.getFromAirport()) && row.getSecondArrAirport().equals(jr.getToAirport()))
+				.filter(row -> row.getDifferenceOfCriticalTime() >= 2 && row.getDifferenceOfCriticalTime() <= 8)
+				.forEachOrdered(row -> lhm.add(row));
+
+		assertEquals(1, lhm.size());
+
+
+	}
+
+
+	@Test
+	public void testJoinRequest2(){
+
+		JoinRequest jr1 = new JoinRequest();
+		jr1.setFromAirport("BOM");// Lombok Setter
+		jr1.setToAirport("JFK");
+		JoinRequest jr2 = new JoinRequest("IAH", "DXB");
+
+		assertEquals(false, jr1.equals(jr2));
+
+		Integer hashCode = new Integer(jr1.hashCode());
+		assertNotNull(hashCode);
+		assertNotNull(jr2.toString());
+
+	}
+
+	@Test
+	public void mockTest(){
+		assertEquals(true, true);
+	}
+
 
 
 }
---
+++ src/test/java/fahmid/islam/controller/FlightScheduleControllerTests.java
@@ -0,0 +1,10 @@
+package fahmid.islam.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+public class FlightScheduleControllerTests {
+
+
+    
+}
